3f84a276652e406344b746c3966bd989c313479b
MazenAtlam <mazenatef5510@gmail.com>
2024-06-27 06:19:42 +0300

    Task 7 version 1.2

--- models/user.py
+++ models/user.py
@@ -10,7 +10,7 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    users_id = Column(Integer, PrimaryKey=True)
+    users_id = Column(Integer, primary_key=True)
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
