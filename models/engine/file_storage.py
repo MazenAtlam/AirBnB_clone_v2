4970da198e129176fa42f73f6a2e2395e98e12b8
MazenAtlam <mazenatef5510@gmail.com>
2024-06-28 08:06:43 +0300

    Convert 'is'&'is not' to '=='&'!=' in console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage
-        
+
         Args:
             cls (class, optional): If spicified, the method should return
             only the objects of one type of cls
