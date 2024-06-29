bdbba361386992b028d588c511df3d04ec2eb6cf
AhmedShehab1 <legendbtw2003@gmail.com>
2024-06-29 07:14:51 +0000

    .

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,9 @@
 from .config import ENV_VAR
 if (ENV_VAR['hbnb_storage_type'] == 'db'):
         from .engine.db_storage import DBStorage
-        storage = DBStorage()
-        storage.reload()
-elif (ENV_VAR['hbnb_storage_type'] == 'file'):
+        storage = DBStorage(
+else:
         from .engine.file_storage import FileStorage
         storage = FileStorage()
-        storage.reload()
+
+storage.reload()
