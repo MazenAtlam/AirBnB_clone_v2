6f4eb0fef94520a55ade70c9148d1235a8d86b85
MazenAtlam <mazenatef5510@gmail.com>
2024-06-29 11:56:08 +0300

    ........

--- models/place.py
+++ models/place.py
@@ -19,9 +19,6 @@ class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     if ENV_VAR['hbnb_storage_type'] == "db":
-        id = BaseModel.id
-        created_at = BaseModel.created_at
-        updated_at = BaseModel.updated_at
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
