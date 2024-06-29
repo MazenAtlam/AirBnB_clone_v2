6cf8540cae570e144eff0c31bf510a1d97ab5f15
MazenAtlam <mazenatef5510@gmail.com>
2024-06-29 10:17:49 +0300

    ..

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,7 @@
 from .config import ENV_VAR
 if (ENV_VAR['hbnb_storage_type'] == 'db'):
         from .engine.db_storage import DBStorage
-        storage = DBStorage(
+        storage = DBStorage()
 else:
         from .engine.file_storage import FileStorage
         storage = FileStorage()
