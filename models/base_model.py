c6f987173e5970cdb06780ffe55507a0841ece63
AhmedShehab1 <legendbtw2003@gmail.com>
2024-06-29 07:55:15 +0000

    .

--- models/base_model.py
+++ models/base_model.py
@@ -51,11 +51,8 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        try:
-            if not dictionary.get("_sa_instance_state"):
-                del dictionary["_sa_instance_state"]
-        except Exception:
-            pass
+        if dictionary.get("_sa_instance_state") is not None:
+            del dictionary["_sa_instance_state"]
         return dictionary
 
     def delete(self):
